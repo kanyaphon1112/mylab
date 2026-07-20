b0252ce33f81dfdde5db6e74daafb4a65e62b464
Kanyaphon Chaichana <kanyaporn4890@gmail.com>
2026-07-20 11:23:55 +0700

    a

--- web/view.py
+++ web/view.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 
+
 def home(request):
-    return render(request, 'myabb/home.html')
+    import datetime
+
+    context = {
+        "title": "My Home Page",
+    }
+    context["date"] = datetime.date.today()
+    return render(request, "myabb/home.html", context)
+
 
 def about(request):
-    return render(request, 'myabb/about.html')
+    return render(request, "myabb/about.html")
+
 
 def contact(request):
-    return render(request, 'myabb/contact.html')
+    return render(request, "myabb/contact.html")
