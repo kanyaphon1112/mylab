00bb05e9880d89ceff51120333e4317e89807a71
Kanyaphon Chaichana <kanyaporn4890@gmail.com>
2026-07-20 10:06:40 +0700

    A

--- myabb/settings.py
+++ myabb/settings.py
@@ -15,8 +15,6 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_DIR = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [STATIC_DIR]
 
 
 # Quick-start development settings - unsuitable for production
