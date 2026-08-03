f2b65cea0c5be35c9ca44e736162a733f17e3818
Kanyaphon Chaichana <kanyaporn4890@gmail.com>
2026-08-03 11:21:31 +0700

    A

--- web/models.py
+++ web/models.py
@@ -1,9 +1,16 @@
 from django.urls import reverse
 from django.db import models
+from django.contrib import admin
+
+PREFIX = [
+    ("นาย", "นาย"),
+    ("นางสาว", "นางสาว"),
+    ("นาง", "นาง"),
+]
 
 
 class student(models.Model):
-
+    prefix_name = models.CharField(max_length=100, blank=False, choices=PREFIX)
     stu_id = models.CharField(max_length=12, unique=True)
     fname = models.CharField(max_length=100, blank=False)
     lname = models.CharField(max_length=100, blank=False)
@@ -13,3 +20,10 @@ class student(models.Model):
 
     def get_absolute_url(self):
         return reverse("student_detail", kwargs={"pk": self.pk})
+
+
+class StudentsAdmin(admin.ModelAdmin):
+    list_display = ("stu_id", "prefix_name", "fname", "lname")
+
+
+admin.site.register(student, StudentsAdmin)
